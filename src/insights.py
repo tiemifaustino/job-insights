e8c908d2761142008b3e661c901e438639b76b83
Tiemi Faustino <hayashipharma@gmail.com>
2022-10-20 19:22:10 -0300

    feat: :white_check_mark: 4 - Implements function 'get_max_salary'

--- src/insights.py
+++ src/insights.py
@@ -59,21 +59,14 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
+    jobs_read = read(path)
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+    max_salary = [
+        int(salary["max_salary"])
+        for salary in jobs_read
+        if salary["max_salary"] and salary["max_salary"] != "invalid"
+    ]
+    return max(max_salary)
 
 
 def get_min_salary(path):
