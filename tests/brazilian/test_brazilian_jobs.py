393bbbb013cd748126ebbcd0535b803c845e0080
Tiemi Faustino <hayashipharma@gmail.com>
2022-10-21 19:47:49 -0300

    test: :test_tube: 11 - Implements test for function 'read_brazilian_file'

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,9 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    brazilian_jobs = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    for job in brazilian_jobs:
+        assert "title" in job
+        assert "salary" in job
+        assert "type" in job
